169a3b3ebb54fd53292925243ad842ba29e17e6f
bennymathew <benny.mathew@student.moringaschool.com>
2023-10-07 15:32:02 +0300

    Practice

---
+++ index.js
@@ -0,0 +1,11 @@
+function addNumbers(num1, num2){
+    let addedNumbers = num1 + num2;
+    return addedNumbers;
+}
+
+const multipliedNumbers = function(num1, num2){
+    let multipliedNumbers = num1 * num2;
+        return multipliedNumbers;
+} 
+
+const multiplication = multipliedNumbers(5,6)
